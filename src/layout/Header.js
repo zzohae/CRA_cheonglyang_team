38aad055ff7e8b623d3367bc42c8386a83dc965e
zzohae <badahaeyeoncho@gmail.com>
2024-12-10 18:12:33 +0900

    [2.5.3] Header, Footer 수정, 스타일 조정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,5 +1,5 @@
 // react
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 // modules
 import { Link } from 'react-router-dom'
 // data
@@ -9,24 +9,39 @@ import Util from '../component/Util';
 import { Btn } from '../component/commonUI';
 // svg
 import { ReactComponent as Logo } from '../asset/svg/common/BI_desktop_Default.svg';
+import { ReactComponent as Logomin } from '../asset/svg/common/logo_min.svg';
 import { ReactComponent as Delivericon } from '../asset/svg/truck.svg'
 import { ReactComponent as Menuline } from '../asset/svg/common/menu_line.svg'
+import { ReactComponent as Menumbline } from '../asset/svg/common/menu_mobile.svg'
 // Components
 import Searchbox from '../component/Searchbox';
+import Topad from '../contents/Topad';
+import Mbtab from './Mbtab'
 
-export default function Header({ keyword, setKeyword, incartNum }) {
+export default function Header({ keyword, setKeyword, incartNum, isLoggedIn, setIsLoggedIn }) {
+  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
   return (
-    <header className='d-flex align-items-center'>
+    <header className='d-flex align-items-center justify-content-center'>
+      <Topad></Topad>
       <div className="hdtop container d-flex justify-content-between align-items-center">
-        <h1 className='order-1 order-lg-0'><a href="/" className='d-flex justify-content-center align-items-center'><Logo width='150' height='53.708'></Logo></a></h1>
+        <h1 className='order-1 order-lg-0'><a href="/" className='d-flex justify-content-center align-items-center'><Logo width='120' height='60'></Logo></a></h1>
         <Searchbox className='order-0 order-lg-1' keyword={keyword} setKeyword={setKeyword}></Searchbox>
-        <Util className='order-2 util d-none d-lg-flex align-items-center justify-content-end' incartNum={incartNum}></Util>
-        <Menuline width='28' height='28' className='mobileallmenu d-block d-lg-none order-3'></Menuline>
+        <Util className='order-2 util d-none d-lg-flex align-items-center justify-content-end' incartNum={incartNum} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></Util>
+        <Menumbline
+            width="28"
+            height="28"
+            className="mobileallmenu d-block d-lg-none order-3"
+            onClick={toggleMenu} />
       </div>
       <div className="hdbtm container">
         <nav className="gnb d-none d-lg-flex justify-content-between align-items-center">
             <div className='mainmenu position-relative'>
-              <Link to={allData.navdata.category.linkto} className='category d-flex justify-content-start align-items-center'><Menuline width='20' height='14' className='me-2'></Menuline>{allData.navdata.category.title}</Link>
+              <Link to={allData.navdata.category.linkto} className='category d-flex justify-content-start align-items-center'><Menuline width='25' height='18' className='me-2'></Menuline>{allData.navdata.category.title}</Link>
               <ul className='hovermenu'>
                   {
                     allData.navdata.category.submenu.map((v, i) => {
@@ -58,6 +73,9 @@ export default function Header({ keyword, setKeyword, incartNum }) {
           </div>
         </nav> 
       </div>
+
+      {/* 모바일 메뉴 */}
+      <Mbtab isOpen={isMenuOpen} toggleMenu={toggleMenu}></Mbtab>
     </header>
   )
 }
