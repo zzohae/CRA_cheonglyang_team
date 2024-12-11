32b379df17070b1982b7b8724b6f616e3377f078
zzohae <badahaeyeoncho@gmail.com>
2024-12-11 12:34:29 +0900

    [3.3.0] 공통 제목 컴포넌트 및 Mainpage의 추천상품 제목, Swiper 구현

---
+++ src/ui/MainTitle.js
@@ -0,0 +1,12 @@
+// react
+import React from 'react'
+
+export default function MainTitle({ textColor, children, nomargin }) {
+  return (
+    <div className={`d-flex flex-column ${nomargin ? '' : 'mb50'}`}>
+      <i className="bi bi-circle-fill" style={{ color: textColor, fontSize: '0.75rem'}}></i>
+      <h2 className='maintitle fs-h1'>
+        {children}</h2>
+    </div>
+  )
+}
