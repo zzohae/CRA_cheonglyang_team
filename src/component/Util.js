0bec1a87362d0717250dae175d02c7b9f2496347
zzohae <badahaeyeoncho@gmail.com>
2024-12-11 15:36:47 +0900

    [3.10.0] Util 구현

--- src/component/Util.js
+++ src/component/Util.js
@@ -1,7 +1,188 @@
-import React from 'react'
+// react
+import React, { useState, useEffect } from 'react';
+// modules
+import { Link, useNavigate } from 'react-router-dom';
+// svg
+import { ReactComponent as AlertIcon } from '../asset/svg/common/alert.svg';
+import { ReactComponent as CartIcon } from '../asset/svg/common/cart.svg';
+import { ReactComponent as MypageIcon } from '../asset/svg/common/user.svg';
+import { ReactComponent as LoginIcon } from '../asset/svg/common/login.svg';
+import { ReactComponent as LogoutIcon } from '../asset/svg/common/logout.svg';
 
-export default function Util() {
+const Button = ({ icon, hasBadge, onClick, badgePosition, label }) => (
+  <button
+    className="icon"
+    onClick={onClick}
+    style={{
+      position: 'relative',
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    {React.cloneElement(icon, { style: { color: '#214AEE' } })}
+    {hasBadge && (
+      <span
+        className="badge"
+        style={{
+          position: 'absolute',
+          top: badgePosition?.top || 0,
+          right: badgePosition?.right || 0,
+          backgroundColor: '#214AEE',
+          width: '5px',
+          height: '5px',
+          borderRadius: '50%',
+          zIndex: 1,
+        }}
+      />
+    )}
+  </button>
+);
+
+const Cart = ({ icon, badgePosition, incartNum }) => (
+  <Link to="/cart"
+    className="icon"
+    style={{
+      position: 'relative',
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    {React.cloneElement(icon, { style: { color: '#214AEE' } })}
+    {incartNum.length > 0 && (
+      <span
+        className="badge"
+        style={{
+          position: 'absolute',
+          fontSize: '11px',
+          fontWeight: 500,
+          top: '-5px',
+          right: '-5px',
+          backgroundColor: 'rgb(33, 74, 238)',
+          width: '18px',
+          height: '18px',
+          borderRadius: '50%',
+          zIndex: 1,
+          textAlign: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 0,
+          boxSizing: 'unset',
+          lineHeight: 1
+        }}
+      >{incartNum.length}</span>
+    )}
+  </Link>
+);
+
+const Alert = ({ icon, incartNum }) => (
+  <Link to="/alert"
+    className="icon"
+    style={{
+      position: 'relative',
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    {React.cloneElement(icon, { style: { color: '#214AEE' } })}
+    {incartNum && (
+      <span
+        className="badge"
+        style={{
+          position: 'absolute',
+          fontSize: '11px',
+          fontWeight: 500,
+          top: '-5px',
+          right: '-4px',
+          backgroundColor: 'rgb(33, 74, 238)',
+          width: '18px',
+          height: '18px',
+          borderRadius: '50%',
+          zIndex: 1,
+          textAlign: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 0,
+          boxSizing: 'unset'
+        }}
+      >{incartNum}</span>
+    )}
+  </Link>
+);
+
+
+
+// Util 컴포넌트
+const Util = ({incartNum, className, isLoggedIn, setIsLoggedIn}) => {
+  const [hasAlerts, setHasAlerts] = useState(false);
+  const navigate = useNavigate();
+
+
+  // 로그인/로그아웃
+  useEffect(() => {
+    const session = localStorage.getItem('session');
+    if (session) {
+      setIsLoggedIn(true);
+    }
+  }, [setIsLoggedIn]);
+
+  const handleLoginClick = () => {
+    if (isLoggedIn) {
+      localStorage.removeItem('session');
+      setIsLoggedIn(false);
+      alert('성공적으로 로그아웃 되었습니다.');
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  };
+
+  // 마이페이지/로그인 페이지 이동
+  const handleMypageClick = () => {
+    if (isLoggedIn) {
+      navigate('/mypage');
+    } else {
+      navigate('/login');
+    }
+  };
+
+  // 알림 상태 토글
+  const handleAlertToggle = () => {
+    setHasAlerts(!hasAlerts);
+  };
+
   return (
-    <div>Util</div>
-  )
-}
+    <div className={` ${className}`}>
+      <Alert
+        icon={<AlertIcon />}
+        hasBadge={7}
+        onClick={handleAlertToggle}
+        label="알림"
+      />
+
+      <Cart
+        icon={<CartIcon />}
+        incartNum={incartNum}
+        label="장바구니"
+      />
+
+      <Button
+        icon={<MypageIcon />}
+        onClick={handleMypageClick}
+        label="마이페이지"
+      />
+
+      <Button
+        icon={isLoggedIn ? <LogoutIcon /> : <LoginIcon />}
+        onClick={handleLoginClick}
+        label={isLoggedIn ? '로그아웃' : '로그인'}
+      />
+    </div>
+  );
+};
+
+export default Util;
