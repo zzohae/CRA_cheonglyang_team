96e053790afaa35d9db1a7dbce887e41281611fe
KSH7131 <gimsh7131@gmail.com>
2024-12-12 16:08:51 +0900

    [3.15.0] 카드뉴스 페이지 구현

--- src/section/p04news/C4cardnews.js
+++ src/section/p04news/C4cardnews.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import EventCard from '../../component/EventCard';
+import { supabase2 } from '../../api/dbConnect';
+import MyPagination from '../../component/MyPagination';
 
 export default function C4cardnews() {
+  const [events, setEvents] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const eventsPerPage = 9;
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data, error } = await supabase2
+          .from('cardNews')
+          .select('*')
+          .order('id', { ascending: true });
+        if (error) throw error;
+        setEvents(data);
+      } catch (error) {
+        console.error('Error fetching events:', error.message);
+      }
+    };
+    fetchEvents();
+  }, []);
+
+  const totalPages = Math.ceil(events.length / eventsPerPage);
+  const indexOfLastEvent = currentPage * eventsPerPage;
+  const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+  const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
   return (
-    <div>C4cardnews</div>
+    <div>
+      <div className="row gx-4 gy-4">
+        {currentEvents.map((event) => (
+          <EventCard
+            key={event.id}
+            imgSrc={`/asset/img/cardnews/cardnews_${event.img_number}.jpg`}
+            title={event.title}
+            period={event.period}
+            isExpired={event.isExpired}
+          />
+        ))}
+      </div>
+
+      {/* Pagination */}
+      <MyPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+    </div>
   )
 }
