5f7d1e2cd1230dd2b382526b42d001534a993c7d
zzohae <badahaeyeoncho@gmail.com>
2024-12-12 15:27:26 +0900

    [3.12.1] 공지사항 페이지 아키텍처를 위한 컴포넌트 임시 제작

---
+++ src/section/p04news/C2online.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function C2online() {
+  return (
+    <div>C2online</div>
+  )
+}
---
+++ src/section/p04news/C4cardnews.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function C4cardnews() {
+  return (
+    <div>C4cardnews</div>
+  )
+}
