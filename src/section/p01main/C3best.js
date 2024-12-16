b8fff7eab1e24e62035f9f452a0d32e443f57d52
zzohae <badahaeyeoncho@gmail.com>
2024-12-16 12:54:20 +0900

    [3.20.5] 메인페이지 베스트 섹션 탭 추가

--- src/section/p01main/C3best.js
+++ src/section/p01main/C3best.js
@@ -9,6 +9,11 @@ import BestProduct from '../../component/BestProduct'
 
 export default function C3best({incartNum, setIncartNum}) {
   const [bestProducts, setBestProducts] = useState([]);
+  const [selectedTab, setSelectedTab] = useState('mealKit');
+
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,8 +39,11 @@ export default function C3best({incartNum, setIncartNum}) {
           return acc;
         }, {});
 
-        // 2. 상위 3개 제품 선택
-        const sortedProducts = productData
+        // 2. 카테고리에 해당하는 제품만 필터링
+        const filteredProducts = productData.filter(product => product.category === selectedTab);
+
+        // 3. 상위 3개 제품 선택
+        const sortedProducts = filteredProducts
           .map((product) => ({
             ...product,
             reviewCount: productReviewCount[product.id] || 0,
@@ -43,13 +51,13 @@ export default function C3best({incartNum, setIncartNum}) {
           .sort((a, b) => b.reviewCount - a.reviewCount)
           .slice(0, 3);
 
-        // 3. 베스트 제품에 대한 평균 평점
+        // 4. 베스트 제품에 대한 평균 평점
         const productRatings = reviewData.reduce((acc, review) => {
           acc[review.productID] = (acc[review.productID] || []).concat(review.rating);
           return acc;
         }, {});
 
-        // 4. 평균 평점을 제품 데이터에 결합
+        // 5. 평균 평점을 제품 데이터에 결합
         const productsWithRatings = sortedProducts.map((product) => {
           const productReviews = productRatings[product.id] || [];
           const avgRating = productReviews.length
@@ -69,12 +77,27 @@ export default function C3best({incartNum, setIncartNum}) {
     };
 
     fetchData();
-  }, []);
+  }, [selectedTab]);
 
   return (
     <div className='container d-flex flex-row align-items-start justify-content-center bestCont'>
       <div className='row'>
         <MainTitle textColor='#214aee' h2size='34px'>{allData.mainPagetitle[1].title}</MainTitle>
+        <ul className='d-flex justify-content-start mytab border-none m-0'>
+          {/* 임시 - 주문 데이터를 만들게 된다면 카테고리별 주문수가 많은 순서대로 4개 카테고리를 잘라 선정 */}
+          <li
+          onClick={() => handleTabClick('mealKit')}
+          className={selectedTab === 'mealKit' ? 'active' : ''}># 밀키트</li>
+          <li
+          onClick={() => handleTabClick('side')}
+          className={selectedTab === 'side' ? 'active' : ''}># 반찬·양념류</li>
+          <li
+          onClick={() => handleTabClick('vegetables')}
+          className={selectedTab === 'vegetables' ? 'active' : ''}># 야채 · 채소</li>
+          <li
+          onClick={() => handleTabClick('fruits')}
+          className={selectedTab === 'fruits' ? 'active' : ''}># 과일</li>
+        </ul>
         <div className='row align-items-stretch m-0 p-0'>
         {bestProducts.map((v, index) => (
           <BestProduct
